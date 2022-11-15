1fc540b0e185957848bd7fb43f719c29003b965d
Gabriel de Lima Couto <gabriel.couto@hitbel.com.br>
2022-11-14 22:17:32 -0300

    inicia router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import routes from './routes';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./views/register";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Register/>} />
+            {routes.map((prop, key) => {
+              return (
+                <Route
+                  path={prop.path}
+                  element={prop.component}
+                  key={key}
+                />
+              );
+            })}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Dashboard from "./views/dashboard";
+
+const routes = [
+    {
+        path: "/dashboard",
+        name: "Dashboard",
+        component: <Dashboard/>
+    }
+]
+
+export default routes;
