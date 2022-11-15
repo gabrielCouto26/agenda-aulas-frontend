2626f56e5ef0338cc97501f10b1dad2a317eadb3
Gabriel de Lima Couto <gabriel.couto@hitbel.com.br>
2022-11-14 22:47:41 -0300

    adiciona container

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import routes from './routes';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./views/register";
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Register/>} />
-            {routes.map((prop, key) => {
-              return (
-                <Route
-                  path={prop.path}
-                  element={prop.component}
-                  key={key}
-                />
-              );
-            })}
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Container>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Register/>} />
+              {routes.map((prop, key) => {
+                return (
+                  <Route
+                    path={prop.path}
+                    element={prop.component}
+                    key={key}
+                  />
+                );
+              })}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
