8ac65473dba241f72d6f359ccb526f3b5e12cd7f
Gabriel Couto <gabrielcouto26@gmail.com>
2022-11-20 17:27:46 -0300

    implementa view NotFound

--- src/routes.js
+++ src/routes.js
@@ -4,38 +4,49 @@ import NewClass from "./views/newClass";
 import MyClasses from "./views/myClasses";
 import ClassDetails from "./views/classDetails";
 import Profile from "./views/profile";
+import NotFound from "./views/notFound";
+
+import { Navigate } from "react-router-dom";
 
 const routes = [
-    {
-      path: "/register",
-      name: "Register",
-      component: <Register/>
-    },
-    {
-      path: "/profile/:id",
-      name: "Profile",
-      component: <Profile/>
-    },
-    {
-      path: "/dashboard",
-      name: "Dashboard",
-      component: <Dashboard/>
-    },
-    {
-      path: "/class/new",
-      name: "New Class",
-      component: <NewClass/>
-    },
-    {
-      path: "/class/list",
-      name: "My Classes",
-      component: <MyClasses/>
-    },
-    {
-      path: "/class/:id/details",
-      name: "Class Details",
-      component: <ClassDetails/>
-    }
+  {
+    path: "*",
+    component: <NotFound />
+  },
+  {
+    path: "/login",
+    component: <Navigate to="/" />
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: <Register />
+  },
+  {
+    path: "/profile/:id",
+    name: "Profile",
+    component: <Profile />
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: <Dashboard />
+  },
+  {
+    path: "/class/new",
+    name: "New Class",
+    component: <NewClass />
+  },
+  {
+    path: "/class/list",
+    name: "My Classes",
+    component: <MyClasses />
+  },
+  {
+    path: "/class/:id/details",
+    name: "Class Details",
+    component: <ClassDetails />
+  }
 ]
 
 export default routes;
